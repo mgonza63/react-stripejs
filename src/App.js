42d1f4774dddc4cd7eea374ab87b4109fca86ef9
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-29 23:21:43 -0500

    created the button and the stripe checkout set appears when clicked

--- src/App.js
+++ src/App.js
@@ -15,17 +15,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
+        <StripeCheckout
+        stripeKey=''
+        token=''
+        name='Buy React'
+        > <button className="btn btn-success">Buy React for $10</button> </StripeCheckout>
       </header>
     </div>
   );
